e3b7d277b56b9865a70c5894b2901d01473cd37f
YashMangal23 <yash1855.becse24@chitkara.edu.in>
2025-03-10 18:57:26 +0530

    Solution to Create a Program to Store and Display Student Records

--- Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
+++ Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
@@ -1,22 +1,41 @@
-#include<stdio.h>
-int main()
-{
-    struct student{
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_STUDENTS 100
+
+struct Student {
     int rollno;
+    float marks;
     char name[50];
-    float attendence;
 };
-struct student stud[1000];
-int n;
-scanf("%d",&n);
-for(int i=0;i<n;i++){
-    scanf("%d",&stud[i].rollno);
-    scanf("%s",&stud[i].name);
-    scanf("%f",&stud[i].attendence);
-}
-for(int i=0;i<n;i++){
-    printf("Roll Number: , Name: , Marks: %d\n",stud[i].rollno,stud[i].name,&stud[i].attendence);
-    
-}
-return 0;
+
+int main() {
+    struct Student students[MAX_STUDENTS];
+    int n;
+
+    printf("Enter the number of students: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("\nEnter details for student %d:\n", i + 1);
+
+        printf("Roll No.: ");
+        scanf("%d", &students[i].rollno);
+
+        printf("Marks: ");
+        scanf("%f", &students[i].marks);
+
+        printf("Name: ");
+        scanf("%s", students[i].name);
+    }
+
+    printf("\nStudent Records:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Roll No.: %d\n", students[i].rollno);
+        printf("Marks: %.2f\n", students[i].marks);
+        printf("Name: %s\n", students[i].name);
+        printf("\n");
+    }
+
+    return 0;
 }
